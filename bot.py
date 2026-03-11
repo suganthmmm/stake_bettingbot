92d5bdde9bcced2c3364880a82b624b7f66a7435
suganthmmm <85300808+suganthmmm@users.noreply.github.com>
2026-03-11 20:32:28 +0530

    Update TELEGRAM_TOKEN to use environment variable

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ from telegram.ext import (
 # ─────────────────────────────────────────────
 #  CONFIG
 # ─────────────────────────────────────────────
-TELEGRAM_TOKEN = os.getenv("7744782372:AAHSKutyK17ZWseX_4lt7rihwLQeADR-9nM", "8789475945:AAGDMLVDDaS3NIzPNQ46jNtKXtUCEt5xMIU")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
 STAKE_API_URL  = "https://api.stake.com/graphql"
 DATA_FILE      = "users.json"
 
@@ -61,7 +61,7 @@ CURRENCIES = ["btc", "eth", "ltc", "doge", "trx", "usdt", "bnb", "xrp"]
     DEPOSIT_CURRENCY,
     # Withdraw
     WITHDRAW_CURRENCY, WITHDRAW_AMOUNT, WITHDRAW_ADDRESS, WITHDRAW_2FA, CONFIRM_WITHDRAW,
-) = range(16)
+) = range(17)
 
 # ─────────────────────────────────────────────
 #  PERSISTENCE
